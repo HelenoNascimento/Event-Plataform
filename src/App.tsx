df52f0b016002bb5ebdfb85c550d48cd003afaec
Heleno Nascimento <heleno_55@hotmail.com>
2022-06-21 22:39:39 -0300

    consumindo uma API e listando as aulas

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { gql, useQuery } from "@apollo/client"
+import { useEffect } from "react"
+
+import { client } from "./lib/apollo"
+import { Event } from "./pages/Event";
+
+
+
+function App() {
+  return(
+ <div>
+  <Event/>
+ </div>
+ )}
+
+export default App
